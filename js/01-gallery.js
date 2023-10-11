de0e17ad1e3631f9c305b61df5d64b6829f65bd2
Lapamaryna <lapamaryna@i.ua>
2023-10-11 16:47:07 +0300

    11.10.2023

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,8 @@
 import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from 'basiclightbox'
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryList = document.querySelector('.gallery');
 
@@ -23,3 +24,25 @@ function createMarkup(arr) {
 // console.log(createMarkup(galleryItems));
 
 galleryList.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+
+galleryList.addEventListener('click', handlerZoom);
+
+function handlerZoom(evt) {
+    evt.preventDefault();
+   
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(evt.target.dataset.source)
+    
+    const instance = basicLightbox.create(`
+        <div class="modal">
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+        </div>
+    `)
+//     const instance = basicLightbox.create(`
+//     <img src="${evt.target.dataset.source}" width="800" height="600">
+// `)
+    instance.show()
+  
+}
