9ab27cf2f0b26de92d81b3270fb20288f9cc0bb4
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2023-10-11 20:31:11 +0300

    Task - 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,7 @@ const galleryList = document.querySelector('.gallery');
 function createMarkup(arr) {
     return arr.map(({ preview, original, description }) => `  
         <li class="gallery__item">
-            <a class="gallery__link" href="${original}">
+            <a class="gallery__link js-gallery-item" href="${original}">
                 <img
                 class="gallery__image"
                 src="${preview}"
@@ -33,16 +33,33 @@ function handlerZoom(evt) {
     if (!evt.target.classList.contains('gallery__image')) {
         return;
     }
-    console.log(evt.target.dataset.source)
+    // console.log(evt.target.dataset.source)
+
+    // if (evt.target === evt.currentTarget) {
+    //     return;
+    // }
+
+    // const currentTarget = evt.target.closest('.js-gallery-item')
     
     const instance = basicLightbox.create(`
         <div class="modal">
         <img src="${evt.target.dataset.source}" width="800" height="600">
         </div>
-    `)
+    `, {
+        onShow: (instance) => {
+            document.addEventListener('keydown', handlerKey);   
+        },
+    })
 //     const instance = basicLightbox.create(`
 //     <img src="${evt.target.dataset.source}" width="800" height="600">
 // `)
+    
     instance.show()
   
+    function handlerKey(e) {
+        // console.log(e)
+        if (e.code === 'Escape') {
+            instance.close() 
+        }
+    }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ const galleryList = document.querySelector('.gallery');
 function createMarkup(arr) {
     return arr.map(({ preview, original, description }) => `
         <li class="gallery__item">
-            <a class="gallery__link" href="${original}">
+            <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
                 <img class="gallery__image"
                 src="${preview}" 
                 alt="${description}" />
@@ -19,3 +19,14 @@ function createMarkup(arr) {
 }
 
 galleryList.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+
+const options = {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+};
+
+let gallery = new SimpleLightbox('.gallery a', options);
